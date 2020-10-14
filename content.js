1a8f3f50f8896e59e7499434af5699ba04f40a03
Takumi Mori <tak@TakuminoMacBook-Air.local>
2020-10-15 03:57:36 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,32 @@
+const ids = [
+  12470,12472,12473,12474,12475,12477,12479,12566,12567 // ,15286,15325,15326
+]
+
+const dark = () => {
+  new Darkmode()
+}
+
+const deleteCourses = (ids) => {
+  document.querySelectorAll('li.type_system ul li.type_course').forEach((li)=>{
+    if(!ids.includes(Number(li.getElementsByTagName('a')[0].getAttribute('href').split('=').pop()))){
+      li.remove()
+    }
+  })
+}
+
+const deleteAdminMenu = () => {
+  document.getElementById('inst32')?.remove()
+}
+
+const deleteCourseCategories = () => {
+  document.getElementById('frontpage-category-names')?.remove()
+}
+
+const main = () => {
+  deleteCourses(ids)
+  deleteAdminMenu()
+  deleteCourseCategories()
+  dark()
+}
+
+window.addEventListener('load', main);
